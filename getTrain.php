9f8108569d2228f886805347a7f9d8077c154e83
cokeIII <coke2484@gmail.com>
2022-07-01 11:27:09 +0700

    f

--- getTrain.php
+++ getTrain.php
@@ -2,16 +2,20 @@
 header('Content-Type: text/html; charset=UTF-8');
 session_start();
 $people_id = $_SESSION["people_id"];
+$users = $_SESSION["people_name"];
 require_once "connect.php";
 require_once "function.php";
 $sql = "select * from train t
 inner join pass_users p on p.train_id = t.train_id
 where p.people_id = '$people_id'
 ";
+if ($users == "admin") {
+    $sql = "select * from train";
+}
 $res = mysqli_query($conn, $sql);
 $data = array();
 $i = 0;
-$data["data"][$i]["no"] ="ไม่พบรายการอบรม";
+$data["data"][$i]["no"] = "ไม่พบรายการอบรม";
 $data["data"][$i]["train_name"] = "";
 $data["data"][$i]["date"] = "";
 $data["data"][$i]["btn_print"] = "";
